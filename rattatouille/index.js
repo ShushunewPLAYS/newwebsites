19419995bbfe276f2d5ad0b66d5dba3158495395
ShushunewPLAYS <66542289+ShushunewPLAYS@users.noreply.github.com>
2022-11-09 18:08:22 -0800

    I added a function and a few more if and else

--- rattatouille/index.js
+++ rattatouille/index.js
@@ -1,7 +1,23 @@
 var sports = prompt("What is the best sport?");
+var food = prompt("What is your favorite food?");
+var math = prompt("What is the most advanced math youve seen")
 
-if(sports === "basketball"){
-  alert("Marrry Me! You win, It is the best! HAHAH")
-} else{
-  alert("WRONG BASKETBALL RULES")
+function bestSport () {
+  if(sports === "basketball"){
+    alert("Marrry Me! You win, It is the best! HAHAH")
+  } else{
+    alert("WRONG BASKETBALL RULES")
+  }
+
+  if(food === "fries"){
+    alert("Fries are the best")
+  } else{
+    alert("Wrong fries are the best!")
+  }
+  if(math === "1+1 "){
+      alert("I agree")
+  } else{
+    alert("I disagrees")
+  }
+
 }
