899ba39ac9b129bf37411dc3df701845fa757cad
ShushunewPLAYS <66542289+ShushunewPLAYS@users.noreply.github.com>
2022-11-14 17:47:04 -0800

    Started christmas project

---
+++ christmas time/index.js
@@ -0,0 +1,27 @@
+function toggleText() {
+
+const giftIdeas = [
+  'bracelet',
+  'computer',
+  'graphic-tees',
+  'switch games',
+  'makeup',
+  'crocs',
+  'socks',
+  'pants',
+  'lip gloss',
+  'books',
+  'candles',
+  'christmas lotion',
+  'face masks',
+];
+
+let randomGift =
+giftIdeas[Math.floor(Math.random()*giftIdeas.length)];
+
+
+
+
+
+
+}
