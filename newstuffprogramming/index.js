e20dabec4281963e1bd8c0802e98031012aa4532
ShushunewPLAYS <66542289+ShushunewPLAYS@users.noreply.github.com>
2022-12-03 09:25:44 -0800

    This will smilar to my merch page, added js

---
+++ newstuffprogramming/index.js
@@ -0,0 +1,10 @@
+var candle = alert("The candle of the century is coming out in 2 hours")
+var candleStuff = prompt("What will you do?  Buy it or save it")
+
+function thatsright(){
+  if(candleStuff === "Buy it"){
+    alert("Wow what a great choice! Credit card loading...")
+  } else{
+    alert("See you soon")
+  }
+}
