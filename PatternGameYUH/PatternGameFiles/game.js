d61bc0a14039376951442e56bc0ba6c52db60c85
ShushunewPLAYS <66542289+ShushunewPLAYS@users.noreply.github.com>
2022-12-11 17:37:40 -0800

    Added jquery and colors

---
+++ PatternGameYUH/PatternGameFiles/game.js
@@ -0,0 +1,12 @@
+$("h1")
+
+
+const gamePattern = [randomChosenColour];
+
+const buttonColours = ["red", "blue", "green", "yellow"]
+var randomChosenColour = randomNumber(buttonColours);
+
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.floor() * 3) + 1;
+}
